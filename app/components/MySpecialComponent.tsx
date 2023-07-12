e7143fbc3b8f8ecf5c21d098132d59a9f7decade
Chung Wu <chungwu@plasmic.app>
2023-07-12 08:56:20 -0700

    Update README on how this works

--- app/components/MySpecialComponent.tsx
+++ app/components/MySpecialComponent.tsx
@@ -1,7 +1,10 @@
-export function MySpecialComponent(props: {className?: string, children?: React.ReactNode}) {
+export function MySpecialComponent(props: {
+  className?: string;
+  children?: React.ReactNode;
+}) {
   return (
     <div className={props.className}>
-      <div>This is my special component</div>
+      <div>This is my special code component</div>
       <div>{props.children}</div>
     </div>
   )
--- app/routes/plasmic-host.tsx
+++ app/routes/plasmic-host.tsx
@@ -2,14 +2,14 @@ import { PlasmicCanvasHost, registerComponent } from "@plasmicapp/react-web/lib/
 import { MySpecialComponent } from "~/components/MySpecialComponent";
 
 export default function PlasmicHost() {
-  registerComponent(MySpecialComponent, {
-    name: "MySpecialComponent",
-    props: {
-      children: "slot"
-    },
-    importPath: "./app/components/MySpecialComponent",
-    importName: "MySpecialComponent"
-  });
-  
   return <PlasmicCanvasHost />
 }
+
+registerComponent(MySpecialComponent, {
+  name: "MySpecialComponent",
+  props: {
+    children: "slot"
+  },
+  importPath: "./app/components/MySpecialComponent",
+  importName: "MySpecialComponent"
+});
